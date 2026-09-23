6d33da51c12df2446cc184f0850e19249f7641d5
Nitin Kumar <nitin.kr001@gmail.com>
2026-09-23 13:05:50 +0530

    tests running but failing

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub struct KvStore{
+    key:   String,
+    value: String,
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        unimplemented!();
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!();
+    }
+
+    pub fn get(&self, key: String) -> Option<String>{
+        unimplemented!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        unimplemented!();
+    }
+}
