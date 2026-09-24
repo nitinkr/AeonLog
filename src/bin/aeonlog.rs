00d5c306c2d0ea8c209fed7803731e7c7f6d4d0d
Nitin Kumar <nitin.kr001@gmail.com>
2026-09-25 01:04:49 +0530

    initial tests passing

--- src/bin/aeonlog.rs
+++ src/bin/aeonlog.rs
@@ -1,3 +1,43 @@
+use std::process;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Set {
+        key: String,
+        value: String,
+    },
+
+    Get {
+        key: String,
+    },
+    
+    Rm {
+        key: String,
+    },
+}
+
 fn main() {
-    println!("Hi, bitcask world!");
+    let args = Cli::parse();
+    println!("{args:?}");
+
+    match args.command {
+        Commands::Set{key: _, value: _} => {
+            eprintln!("unimplemented");
+        },
+        Commands::Get{key: _} => {
+            eprintln!("unimplemented");
+        },
+        Commands::Rm{key: _} => {
+            eprintln!("unimplemented");
+        }
+    }
+    process::exit(1);
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,26 +3,26 @@ use aeonlog::KvStore;
 use predicates::str::contains;
 use std::process::Command;
 
-// `kvs` with no args should exit with a non-zero code.
+// `aeonlog` with no args should exit with a non-zero code.
 #[test]
 fn cli_no_args() {
     Command::cargo_bin("aeonlog").unwrap().assert().failure();
 }
 
-// `kvs -V` should print the version
+// `aeonlog -V` should print the version
 #[test]
 fn cli_version() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["-V"])
         .assert()
         .stdout(contains(env!("CARGO_PKG_VERSION")));
 }
 
-// `kvs get <KEY>` should print "unimplemented" to stderr and exit with non-zero code
+// `aeonlog get <KEY>` should print "unimplemented" to stderr and exit with non-zero code
 #[test]
 fn cli_get() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["get", "key1"])
         .assert()
@@ -30,10 +30,10 @@ fn cli_get() {
         .stderr(contains("unimplemented"));
 }
 
-// `kvs set <KEY> <VALUE>` should print "unimplemented" to stderr and exit with non-zero code
+// `aeonlog set <KEY> <VALUE>` should print "unimplemented" to stderr and exit with non-zero code
 #[test]
 fn cli_set() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["set", "key1", "value1"])
         .assert()
@@ -41,10 +41,10 @@ fn cli_set() {
         .stderr(contains("unimplemented"));
 }
 
-// `kvs rm <KEY>` should print "unimplemented" to stderr and exit with non-zero code
+// `aeonlog rm <KEY>` should print "unimplemented" to stderr and exit with non-zero code
 #[test]
 fn cli_rm() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["rm", "key1"])
         .assert()
@@ -54,13 +54,13 @@ fn cli_rm() {
 
 #[test]
 fn cli_invalid_get() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["get"])
         .assert()
         .failure();
 
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["get", "extra", "field"])
         .assert()
@@ -69,19 +69,19 @@ fn cli_invalid_get() {
 
 #[test]
 fn cli_invalid_set() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["set"])
         .assert()
         .failure();
 
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["set", "missing_field"])
         .assert()
         .failure();
 
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["set", "extra", "extra", "field"])
         .assert()
@@ -90,13 +90,13 @@ fn cli_invalid_set() {
 
 #[test]
 fn cli_invalid_rm() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["rm"])
         .assert()
         .failure();
 
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["rm", "extra", "field"])
         .assert()
@@ -105,7 +105,7 @@ fn cli_invalid_rm() {
 
 #[test]
 fn cli_invalid_subcommand() {
-    Command::cargo_bin("kvs")
+    Command::cargo_bin("aeonlog")
         .unwrap()
         .args(&["unknown", "subcommand"])
         .assert()
